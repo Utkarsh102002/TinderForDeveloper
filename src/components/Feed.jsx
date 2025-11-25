4d4c86eb4cc94f87bcfcc0ac4de23226e5be7320
Utkarsh Shrivastava <128974569+Utkarsh102002@users.noreply.github.com>
2025-11-26 01:18:54 +0530

    Change import path for UserCard component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,7 +4,7 @@ import { BASE_URL } from "../utils/constant.js";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
 import { useEffect } from "react";
-import UserCard from "./UserCard";
+import UserCard from "./UserCard.jsx";
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
